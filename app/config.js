5e24501ce09ea4b84b4ba565e0f183429e0a4434
Christina Gyles <christinagyles@Christinas-MacBook-Pro.local>
2018-09-04 17:12:55 +0100

    add page to collect name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Test prototype',
+  serviceName: 'Apply for a birthday card from the Queen',
 
   // Default port that prototype runs on
   port: '3000',
